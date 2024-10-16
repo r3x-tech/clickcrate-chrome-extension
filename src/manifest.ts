5efcba073695663a4f268f39366d69dc7989e604
adrian-petrykiw <cucumber901@gmail.com>
2024-10-15 23:59:04 -0500

    Update manifest.ts

--- src/manifest.ts
+++ src/manifest.ts
@@ -80,6 +80,6 @@ export default defineManifest({
     'https://docs.google.com/*',
     'https://www.instagram.com/*',
     'https://www.notion.so/*',
-    'https://github.com/*', // Added GitHub URL
+    'https://github.com/*',
   ],
 });
